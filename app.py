93a792dbcc6bd8f2ae55913c929a854b1f02e90a
SpaceSnek <jordan.wentland@gmail.com>
2025-11-19 07:52:02 -0500

    Initial Commit

---
+++ app.py
@@ -0,0 +1,27 @@
+#Import required libraries
+from flask import Flask, send_from_directory, abort, render_template
+
+import random
+import os 
+
+#Initialize Flask app
+app = Flask(__name__)
+fishtank = 'images'
+
+#Random fish API Endpoint 
+@app.route('/fish/')
+def serve_image():
+    try: 
+        random_integer = random.randint(1, 2)
+        return send_from_directory(fishtank, f'{random_integer}.jpg')
+    except FileNotFoundError:
+        abort(404)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+#Run the app
+if __name__ == '__main__':
+    os.makedirs(fishtank, exist_ok=True) # Make img directory if it doesnt exist, need a tank for da fish
+    app.run(debug=True) # Run in debug mode for development
