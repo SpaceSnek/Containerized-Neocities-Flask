88c9ea7cf3b8002c0997e1a028876e555fd3946f
SpaceSnek <jordan.wentland@gmail.com>
2025-11-25 11:20:18 -0500

    atproto update

--- atproto_test.py
+++ atproto_test.py
@@ -4,7 +4,7 @@ client = Client()
 client.login(credentials.username, credentials.password)
 target_handle = 'spacesnek.shrimplybetter.me'
 
-feed = client.app.bsky.feed.get_author_feed({'actor': target_handle, 'limit': 5})
+feed = client.app.bsky.feed.get_author_feed({'actor': target_handle, 'limit': 1})
 formattedPost = []
 for post_view in feed['feed']:
     try:
@@ -17,7 +17,7 @@ for post_view in feed['feed']:
             pass
         author = post_view.post.author.handle
         postText = record.text
-        postTime = record.created_at
-        formattedPost.append((author, postText, embed, postTime))
+        formattedPost.append((author, postText, embed))
     except KeyError:
         formattedPost = "This post has been deleted or is not displaying properly."
+print(f"{formattedPost[0][0]}: \"{formattedPost[0][1]}\"\n{formattedPost[0][2]}")
