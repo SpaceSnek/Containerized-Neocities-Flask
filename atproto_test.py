21f53bced21ebde2d55d14c338c8be4d5d4fa3d9
SpaceSnek <jordan.wentland@gmail.com>
2025-11-25 10:27:15 -0500

    atproto_testing

---
+++ atproto_test.py
@@ -0,0 +1,23 @@
+from atproto import Client
+import credentials
+client = Client()
+client.login(credentials.username, credentials.password)
+target_handle = 'spacesnek.shrimplybetter.me'
+
+feed = client.app.bsky.feed.get_author_feed({'actor': target_handle, 'limit': 1})
+for post_view in feed['feed']:
+    try:
+        record = post_view.post.record
+        embed = post_view.post.embed
+        try:
+            embed = embed.images[0].thumb
+        except:
+            embed = ""
+            pass
+        author = post_view.post.author.handle
+        postText = record.text
+        postTime = record.created_at
+        formattedPost = (f"Author: {author}\nText: {postText}\nEmbed: {embed}\nPosted at: {postTime}\n")
+    except KeyError:
+        formattedPost = "This post has been deleted or is not displaying properly."
+    print(formattedPost)
